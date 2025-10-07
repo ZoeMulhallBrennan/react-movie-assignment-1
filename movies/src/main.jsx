d6c04f885bb73a84c4223d9db5cfd1dce9a70341
zbrennan5642 <zbrennan5642@gmail.com>
2025-10-07 17:18:30 +0100

    Full review page.

--- movies/src/main.jsx
+++ movies/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
+import MovieReviewPage from "./pages/movieReviewPage";
 
 
 
@@ -22,6 +23,7 @@ const App = () => {
       <Routes>
         <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
+        <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={ <Navigate to="/" /> } />
       </Routes>

fa86eaaa265c42aa7ac39ffa255556e48ba37bbb
zbrennan5642 <zbrennan5642@gmail.com>
2025-10-14 12:34:20 +0100

    Card icons - make action on movie card configurable.

--- movies/src/pages/homePage.jsx
+++ movies/src/pages/homePage.jsx
@@ -3,6 +3,8 @@ import { getMovies } from "../api/tmdb-api";
 import PageTemplate from '../components/templateMovieListPage';
 import { useQuery } from '@tanstack/react-query';
 import Spinner from '../components/spinner';
+import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
+
 
 const HomePage = (props) => {
 
@@ -21,17 +23,20 @@ const HomePage = (props) => {
   
   const movies = data.results;
 
-  // Redundant, but necessary to avoid app crashing.
+
   const favorites = movies.filter(m => m.favorite)
   localStorage.setItem('favorites', JSON.stringify(favorites))
   const addToFavorites = (movieId) => true 
 
-  return (
-    <PageTemplate
-      title='Discover Movies'
-      movies={movies}
-      selectFavorite={addToFavorites}
-    />
+   return (
+      <PageTemplate
+        title="Discover Movies"
+        movies={movies}
+        action={(movie) => {
+          return <AddToFavoritesIcon movie={movie} />
+        }}
+      />
   );
+
 };
 export default HomePage;

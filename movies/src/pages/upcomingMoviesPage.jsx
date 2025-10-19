778a6ba848303b880f6b68005d397a854727ea02
zbrennan5642 <zbrennan5642@gmail.com>
2025-10-19 01:37:25 +0100

    New button for upcoming movies page.

--- movies/src/pages/upcomingMoviesPage.jsx
+++ movies/src/pages/upcomingMoviesPage.jsx
@@ -3,6 +3,7 @@ import PageTemplate from "../components/templateMovieListPage";
 import { useQuery } from "@tanstack/react-query";
 import { getUpcomingMovies } from "../api/tmdb-api";
 import Spinner from "../components/spinner";
+import AddToWatchlist from "../components/cardIcons/addToWatchlist";
 
 const UpcomingMoviesPage = () => {
   const { data: movies, isLoading, error } = useQuery({
@@ -13,13 +14,11 @@ const UpcomingMoviesPage = () => {
   if (isLoading) return <Spinner />;
   if (error) return <p>Something went wrong: {error.message}</p>;
 
-  const toDo = () => true;
-
   return (
     <PageTemplate
       title="Upcoming Movies"
       movies={movies}
-      action={toDo}
+      action={(movie) => <AddToWatchlist movie={movie} />}
     />
   );
 };
